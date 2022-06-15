a37f5f020e1d6ec4f8a73515adf6b7a3e247bb33
samuel <samuel14sromovsky@gmail.com>
2022-06-15 22:29:00 +0200

    another commit. dont know what i did

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  
+  employees: Employee[] = [];
+  
+
+  constructor() {
+    
+
+  }
+   
+
+  ngOnInit(): void {
+
+    this.employees = [
+      {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        emailId: 'sss@sss.com'
+      },
+      {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Doe',
+        emailId: 'ddfj@jf.dsd'
+      }];
+
+  }
+
+}
